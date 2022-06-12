918aefd3c16b83450f21f8d136dbfb55b3ad9e87
oscar <oscar.andres.vargas1@gmail.com>
2022-06-12 14:36:38 -0500

    User routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,17 +3,29 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/getAllUsers', async (req, res) => {
-    let allUsers = await User.find({});
+    try {
+        let allUsers = await User.find({});
 
-    if (allUsers.length > 0) {
-        res.json({ allUsers });
+        if (allUsers.length > 0) {
+            res.json({ allUsers });
+        } else {
+            res.status(404).json({ message: 'Usuarios no encontrados' });
+        }
+    } catch (e) {
+        res.status(400).json({ message: 'Ocurrio un error en el servidor' });
     }
+});
 
-    res.status(404).json({ message: 'No users found' });
-})
-
-router.post('/createUser', (req, res) => {
-
+router.post('/createUser', async (req, res) => {
+    try {
+        let newUser = await new User({
+            name: req.body.name,
+            age: req.body.age,
+        }).save();
+        res.status(201).json({ message: 'User created', newUser });
+    } catch (e) {
+        res.status(400).json({ message: 'Ocurrio un error en el servidor' });
+    }
 });
 
 module.exports = router;
